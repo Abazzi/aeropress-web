ee838dad5ba29e46a29ca399067440fae2569687
Adam Bazzi <adambazzi@proton.me>
2023-06-02 13:40:44 -0400

    Added font credit in readme removed dark classes in html, will be added on toggle removed checked option on input removed font inport in html changed var to let in the diceRolls function started the toggleTheme() function removed lightmode variables and changed some dark mode variables in css

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 Original idea by [James Hoffmann](https://jimseven.com/2017/12/06/coffee-brewing-dice/)
 Colour Schemes from [Happy Hues](https://happyhues.co)
+Oxygen font designed by [Vernon Adams](https://fonts.google.com/specimen/Oxygen)
--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 /**
- * fun diceRolls 
+ * function diceRolls 
  */
+let die;
 
 function diceRolls() {
-    var diceRollsArray = [];
+    let diceRollsArray = [];
 
     for(i = 0; i <= 5; i++){
-        var die = Math.floor(Math.random() * 6);
+        die = Math.floor(Math.random() * 6);
         diceRollsArray.push(die);
     }
 
@@ -27,7 +28,7 @@ function generateRecipe(){
     var temperatureOptions = ["75 C","80 C","85 C","90 C","95 C","Your Choice"];
     var steepTimeGrindOptions = ["Coarse - 4 Minutes","Medium - 120 Seconds","Medium Fine - 90s","Fine - 60s","Very Fine - 60s","Your Choice"];
     var waterToCoffeeOptions = ["12g of Coffee to 200g of Water","15g of Coffee to 200g of Water","15g of Coffee to 250g of Water","24g of Coffee to 200g of Water (Dilute to Share)","30g of Coffee to 200g of Water (Dilute to Share)","Your Choice"];
-    var stirringOptions = ["Stir Once Before Pressing","Stir Twice Before Pressing","Stir Once Clickwise, and Once Counter-Clickwise Before Pressing","Stir North, South, East, West Before Pressing","Don't Stir","Your Choice"];
+    var stirringOptions = ["Stir Once Before Pressing","Stir Twice Before Pressing","Stir Once Clockwise, and Once Counter-Clockwise Before Pressing","Stir North, South, East, West Before Pressing","Don't Stir","Your Choice"];
     var positionBloomOptions = ["Upright, 30s Bloom, 30g of Water","Upright, no Bloom","Inverted, 30s Bloom, 30g of Water","Inverted, no Bloom","Upright, 30s Bloom, 60g of Water","Inverted, 30s Bloom, 60g of Water"];
 
     var recipe = [temperatureOptions[diceRollsArray[0]],steepTimeGrindOptions[diceRollsArray[1]],waterToCoffeeOptions[diceRollsArray[2]],
@@ -64,6 +65,18 @@ function displayRecipe() {
     }
 
     
+}
+
+function toggleTheme(){
+    let classes = ["body","header","subtitle","recipeDiv","footer"]
+    let toggle = document.querySelector('#toggleTheme');
+    if(toggle.checked){
+        for(i = 0; i < classes.length; i++){
+            document.querySelector(`.${classes[i]}`).classList.toggle(`${classes[i]}-dark`);
+        }
+    }else{
+        console.log("Bye");
+    }
 }
 
 // Basic function for the button that calls both clearRecipe() and displayRecipe()
